41b523c6850d0230de9917afc514caa0e015f6bf
eternal-echo <yuanyu@whut.edu.cn>
2024-12-14 14:26:53 +0800

    生成算子工程

---
+++ CANN_C_Operator_S3/argmax_with_value_case/op_kernel/arg_max_with_value_case.cpp
@@ -0,0 +1,6 @@
+#include "kernel_operator.h"
+
+extern "C" __global__ __aicore__ void arg_max_with_value_case(GM_ADDR x, GM_ADDR indices, GM_ADDR values, GM_ADDR workspace, GM_ADDR tiling) {
+    GET_TILING_DATA(tiling_data, tiling);
+    // TODO: user kernel impl
+}
